58862b297c464b1822975c9e83caee3d7d4caa21
THiago Soares Mota <thiagomota456@gmail.com>
2021-12-04 16:51:15 -0300

    Add files

---
+++ js/script.js
@@ -0,0 +1,95 @@
+
+import { Octokit } from "https://cdn.skypack.dev/@octokit/core";
+import { createAppAuth } from "@octokit/auth-app";
+
+let HttpRequest = new XMLHttpRequest()
+
+btnSearch.addEventListener("click", async function(e){
+    e.preventDefault();
+    
+    const octokit = new Octokit({ auth: `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` })
+
+    let query = document.getElementById("searchInput").value 
+    console.log(query)
+
+    const response1 = await octokit.request('GET /search/repositories', {
+        q: query
+    });
+
+    console.log(response1)
+
+    let pesquisa = "";
+    for(let i = 0; i < response1.data.items.length; i++){
+        pesquisa += `<a href="${response1.data.items[i].html_url}">${response1.data.items[i].full_name}</a><br>
+                    <p>${response1.data.items[i].description}</p><br><br>`
+    }
+
+    document.getElementById("pesquisa").innerHTML = pesquisa;
+
+})
+
+window.addEventListener("load", function() {
+    CarregardadosDoPerfil();
+})
+
+function CarregardadosDoPerfil(){
+    HttpRequest.onload = function(){
+        let data = JSON.parse(this.responseText)
+        
+         
+        let paerfilString =`<div class="col-12 col-md-3">
+                                <a href="https://github.com/thiagomota456"><img src=${data.avatar_url} alt=""></a>
+                            </div>
+
+                            <div class="col-12 col-md-9">
+
+                                <div class="row" id="textos-objetivos">
+                                <a href="https://github.com/thiagomota456"><h1>${data.name}a</h1>
+                                    <h2>login: ${data.login}</h2></a>
+
+                                    <a href="https://github.com/thiagomota456"><p><span class="negrito">Bio: </span>${data.bio}</p></a>
+                                </div>
+                                
+                                <div class="redesSociais">
+                                    <h2>Redes Sociais</h2>
+                                    <a href=${data.blog}><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+                                </div>
+
+                            </div>`
+
+        document.getElementById("perfil").innerHTML = paerfilString;
+
+        carregaMeusRepositorios()
+    }
+    HttpRequest.open('GET', 'https://api.github.com/users/thiagomota456');
+    HttpRequest.send();
+}
+
+
+function carregaMeusRepositorios(){
+    HttpRequest.onload = function(){
+
+        let data = JSON.parse(this.responseText)
+        console.log(data)
+
+        let CardRepositoryString = ""
+        for(let i = 0; i < data.length; i++){
+            CardRepositoryString +=`<div class="col-12 col-md-4 Repositorio-card">
+                  
+                                <div class="row">
+                                <i class="fas fa-folder col-2"></i>
+                                <a class=" col-10 linkDoRepositorio" href=${data[i].url}><p>${data[i].name}</p></a>
+                                </div>
+
+                                <p>${data[i].description}</p>
+                                
+                            </div>`
+        }
+         
+        
+
+        document.getElementById("meusRepositorios").innerHTML = CardRepositoryString;
+    }
+    HttpRequest.open('GET', 'https://api.github.com/users/thiagomota456/repos');
+    HttpRequest.send();
+}
